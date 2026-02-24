84eed6a94f2f08c1d70bf750d8cfa80af7702df1
im a rockstar <129412860+historism@users.noreply.github.com>
2026-02-24 16:26:17 +0100

    Add files via upload

--- js/titlebar.js
+++ js/titlebar.js
@@ -5,32 +5,33 @@ const title = "tear"
 const domain = ".wtf"
 
 function SetTitle() {
-    let index = 0;
-    let deleting = false;
+  let index = 0, deleting = false;
+  const fulltext = title + domain;
+  let timer;
 
-    const fulltext = title+domain
+  function tick() {
+    if (!deleting) {
+      document.title = "@" + fulltext.slice(0, index + 1);
+      index++;
+      if (index === fulltext.length) {
+        deleting = true;
+        timer = setTimeout(tick, 1800); 
+        return;
+      }
+    } else {
+      document.title = "@" + fulltext.slice(0, index - 1);
+      index--;
+      if (index === 1) deleting = false;
+    }
+    timer = setTimeout(tick, interval);
+  }
 
-    setInterval(() => {
-        if(!deleting)
-        {
-            // typing
-            document.title = "@" + fulltext.slice(0, index +1) // Starts writing at Ts
-            index++;
+  tick();
 
-            if (index==fulltext.length)
-            {
-                deleting=true;
-                setTimeout(() => {}, 500); // Small delay between writing and removing
-            } 
-        } else {
-            document.title = "@" + fulltext.slice(0, index -1);
-            index--;
-
-            if(index==1){ // Stops deleting at T
-                deleting =false;
-            }
-        }
-    }, interval);
+  document.addEventListener('visibilitychange', () => {
+    if (document.hidden) clearTimeout(timer);
+    else tick();
+  });
 }
 
 SetTitle();
